923167aa9ac1d557d8aae760b0e2830ebdc5dd85
podletsky <nazar.org@gmail.com>
2023-04-05 21:12:30 +0300

    new

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+// // Add imports above this line
+// import { galleryItems } from './gallery-items';
+// // Change code below this line
+import "simplelightbox/dist/simple-lightbox.min.css"
+
+import SimpleLightbox from "simplelightbox"
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const arrayImages = galleryItems.map(({ preview, description, original }) => {
+    return `<li class="gallery__item"><a href='${original}'
+     class="gallery__link"> <img class='gallery__image' src='${preview}'data-source='${original}' alt='${description}' width='340'></img></a></li>`
+}).join('')
+console.log(arrayImages)
+
+const galleryList = document.querySelector('.gallery')
+galleryList.insertAdjacentHTML('afterbegin', arrayImages)
+
+const lightbox = new SimpleLightbox( '.gallery a', {
+    
+  captionDelay: 250,
+  captionsData: "alt",
+})
+console.log('cool')
