eb25aacca14eb786c8919e9bcef1c780be68aae3
podletsky <nazar.org@gmail.com>
2023-04-09 13:07:06 +0300

    video-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,54 @@
 
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe)
+
+const currentTime=(data)=>{
+    localStorage.setItem(
+        'videoplayer-current-time', 
+        JSON.stringify(data.seconds));  
+    let time = localStorage.getItem('videoplayer-current-time');
+};
+
+player.on('timeupdate',throttle(currentTime, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+// // .then(function(seconds){})
+// // .catch(function(error){
+// //     switch(error.name){
+// //         case 'RangeError' : break;
+// //         default: break;
+// //     }
+// // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
